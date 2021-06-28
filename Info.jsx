552b0c316480af68020b211abe974871a7aeed23
sreelekshmisunish <70190142+sreelekshmisunish@users.noreply.github.com>
2021-06-28 11:09:04 +0530

    Add files via upload

---
+++ Info.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Info(){
+  return(
+   <div class = "note">
+     <h1>Web Development Bootcamp</h1>
+     <p>Web development with Javascript and ReactJS</p>
+   </div>
+  );
+}
+
+export default Info;
